41f2e74dcf401ba479d152001c000a6c43b455dc
shunsaku0621 <hamagi.0621@icloud.com>
2021-08-24 23:06:58 +0900

    修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,17 +1,35 @@
 const pay = () => {
-  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY); 
-  const submit = document.getElementById("button");
-  submit.addEventListener("click", (e) => { // イベント発火
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+    const submit = document.getElementById("button"); 
+    submit.addEventListener("click", (e) => { // イベント発火
     e.preventDefault();
-     // カード情報の取得先
-     const formResult = document.getElementById("charge-form");
-     const formData = new FormData(formResult);
- 
-     const card = { // カードオブジェクトを生成
-       number: formData.get("number"),              // カード番号
-       cvc: formData.get("cvc"),                    // カード裏面の3桁の数字
-       exp_month: formData.get("exp_month"),        // 有効期限の月
-       exp_year: `20${formData.get("exp_year")}`,   // 有効期限の年
-     };
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("number"),
+      cvc: formData.get("cvc"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status === 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='card_token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+
+      document.getElementById("number").removeAttribute("name");
+      document.getElementById("cvc").removeAttribute("name");
+      document.getElementById("exp_month").removeAttribute("name");
+      document.getElementById("exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+    });
   });
 };
+
+window.addEventListener("load", pay);
