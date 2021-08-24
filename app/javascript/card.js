5ce54f6a477ec2659188a1eb26280f14281d0276
shunsaku0621 <hamagi.0621@icloud.com>
2021-08-24 22:59:10 +0900

    カード情報を獲得

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,5 +3,15 @@ const pay = () => {
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => { // イベント発火
     e.preventDefault();
+     // カード情報の取得先
+     const formResult = document.getElementById("charge-form");
+     const formData = new FormData(formResult);
+ 
+     const card = { // カードオブジェクトを生成
+       number: formData.get("number"),              // カード番号
+       cvc: formData.get("cvc"),                    // カード裏面の3桁の数字
+       exp_month: formData.get("exp_month"),        // 有効期限の月
+       exp_year: `20${formData.get("exp_year")}`,   // 有効期限の年
+     };
   });
 };
